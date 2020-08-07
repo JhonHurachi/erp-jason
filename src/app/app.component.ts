9e0d561e738d8eafa5f2023c9c006e4d5cef77f5
Jhonatan Tupayachi Hurtado <jhonatantupayachihurtado@outlook.com>
2020-08-06 22:13:01 -0500

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { ExcelService } from './services/excel.service';
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto1';
+  registros$ = this.afs.collection<any>('productos', ref => ref.orderBy('abreviatura', 'asc'))
+      .valueChanges().pipe(catchError(() => of([])))
+      .pipe(
+        map((datos: any[]) => datos.map(reg => ({
+            ...reg
+        })))
+    );
+
+  constructor(
+    private afs: AngularFirestore,
+    private xlsx: ExcelService
+  ) {
+  }
+
+  exportar(registros: any[]) {
+    this.xlsx.exportAsExcelFile(registros, 'reporte');
+  }
+
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDpbHTsc3BzCsc6q7zfwOkPFMiV_J568T0',
+    authDomain: 'appinventor-6864f.firebaseapp.com',
+    databaseURL: 'https://appinventor-6864f.firebaseio.com',
+    projectId: 'appinventor-6864f',
+    storageBucket: 'appinventor-6864f.appspot.com',
+    messagingSenderId: '1055939650840',
+    appId: '1:1055939650840:web:801abdf3b3f7711338aa6d'
+  }
 };
